a1ecb69da9afadca238b99ae1d7a9a8bff8858dd
daniyal ali <daniyalali12568@gmail.com>
2024-02-24 02:55:45 +0500

    operator

--- operators.py
+++ operators.py
@@ -7,7 +7,6 @@ print("Multiplication of two number is ",2*2)
 print("Division of two number is ",4/2)
 print("Modulus of two number is ",4%2)
 print("Exponentiation of two number is ",6**2)
-print("Floor division of two number is ",7%2)
 
 
 # BOOLEAN OPERATOR
@@ -20,6 +19,7 @@ print("OR operator",2==2 or 2==3)
 
 # ASSIGNMENT OPERATOR:
 numAdd = 8.0
+
 numAdd +=2.5
 print(numAdd)
 
