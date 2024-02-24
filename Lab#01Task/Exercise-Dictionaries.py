f071e5ef641798fef81c9ab23235de3552194ce9
daniyal ali <daniyalali12568@gmail.com>
2024-02-25 02:57:19 +0500

    update

--- Lab#01Task/Exercise-Dictionaries.py
+++ Lab#01Task/Exercise-Dictionaries.py
@@ -29,3 +29,17 @@ else:
 # Checking if a key exists in the dictionary
 value1 =my_dict.get("a")
 print("value is associated with keys :",value1)
+
+# (ii)Write a Python script to concatenate following dictionaries to create a new one.
+# Sample Dictionary :
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+# Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+finalDict = {**dic1,**dic2,**dic3}
+print(finalDict)
