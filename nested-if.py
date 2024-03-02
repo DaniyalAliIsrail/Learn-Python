82a587762872eb25d2a62ca6b1b8b304215cb373
daniyal ali <daniyalali12568@gmail.com>
2024-03-02 20:57:38 +0500

    if-else-elif

---
+++ nested-if.py
@@ -0,0 +1,10 @@
+# Nested If
+# You can have if statements inside if statements, this is called nested if statements.
+
+x=40
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20")
