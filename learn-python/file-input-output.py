0e0569912696435ee280b92352e3f8992cc2d056
daniyal ali <daniyalali12568@gmail.com>
2024-03-07 02:17:21 +0500

    file-handling

---
+++ learn-python/file-input-output.py
@@ -0,0 +1,24 @@
+# PYTHON FIle OPEN
+# file handling is an important part of any web application .
+# Python have several function for creating,reading,uploading and deletingfiles .
+
+# FILE HANDLING
+
+# The key function for working with files in python is the open()function.function
+# The open()function takes two parameters: filename and mode.
+# There are four different methods (modes) for opening a file.
+
+# "r" Read default value opens a file for reading, error if the file does not exist
+# "a" Append - Opens a file for appending, creates the file if it does not exist
+# "w" Write - Open a file for writing, creates the file if it doest not exist
+# "x" Create - Creates the specified file, returns an error if the file exist 
+# In addition you can specify if the should handle as binary or text mode 
+# "t" Text-Default value Text mode
+# "b" Binry-Binary mode(e.g images)
+
+
+
+
+
+
+
